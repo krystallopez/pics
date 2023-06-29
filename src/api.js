b74286f329094f0c7a41c1fcc1acb0d9e6f2a5c6
Krystal <lopezkrystal10@gmail.com>
2023-06-29 14:07:14 -0700

    updated searchImages function and removed the import from index

--- src/api.js
+++ src/api.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID BreFeMvyotVPKNbAOc7LZNEfEr6_VtKKx6dYxssdWvM", // ADD ACCESS KEY HERE
     },
     params: {
-      query: 'cars',
+      query: term,
     },
   });
-  console.log(response)
-  return response;
+  return response.data.results;
 };
 
 export default searchImages;
