32ed3046f26eb3b5dd66adc7007f2db285855c3a
Krystal <lopezkrystal10@gmail.com>
2023-07-03 11:39:50 -0700

    updated search bar component with form input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  return <div>App</div>
+  // eslint-disable-next-line no-unused-vars
+  const handleSubmit = (term) => {
+    console.log("do a search with", term);
+  }
+
+    return (
+      <div>
+        <SearchBar onSubmit={handleSubmit} />
+      </div>
+    );
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
+function SearchBar({ onSubmit }) {
+  // eslint-disable-next-line no-unused-vars
+  const [term, setTerm] = useState(""); // state to manage term with input
 
-function SearchBar() {
-  return <div>App</div>
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(term);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
